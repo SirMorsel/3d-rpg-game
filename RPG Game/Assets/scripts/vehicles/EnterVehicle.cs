0f8321da583ef8b337214b2210d08865d2122050
unknown <patrick.nibbia@gmail.com>
2020-09-12 17:38:19 +0200

    feat: add enter vehicles and vehicle behavior

---
+++ RPG Game/Assets/scripts/vehicles/EnterVehicle.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterVehicle : MonoBehaviour {
+    // Script must be in Vehicle
+    public GameObject vehicle;
+    public GameObject player;
+    public GameObject playerBackup;
+
+    private bool inVehicle = false;
+    Vehicle vehicleScrip;
+    GameObject guiObj;
+    AudioSource source;
+    public AudioClip audio;
+
+    public Camera mainCam;
+    public Camera driveCam;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mainCam.enabled = true;
+        driveCam.enabled = false;
+        vehicleScrip = GetComponent<Vehicle>();
+        vehicleScrip.enabled = false;
+        source = GetComponent<AudioSource>();
+        // guiObj = GameObject.Find("PressE");
+        // guiObj.SetActive(false);
+        playerBackup.SetActive(false);
+       // guiObj.SetActive(false);
+        	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (inVehicle == true && Input.GetKey(KeyCode.F))
+        {
+            player.SetActive(true);
+            player.transform.parent = null;
+            playerBackup.SetActive(false);
+            vehicleScrip.enabled = false;
+            inVehicle = false;
+            mainCam.enabled = true;
+            driveCam.enabled = false;
+        }
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && inVehicle == false)
+        {
+          //  guiObj.SetActive(true);
+        }
+
+        if (other.gameObject.tag == "Player" && inVehicle == false && Input.GetKey(KeyCode.E))
+        {
+           // source.PlayOneShot(audio, 1);
+          //  guiObj.SetActive(false);
+            playerBackup.SetActive(true);
+            player.SetActive(false);
+            player.transform.parent = vehicle.transform;
+            vehicleScrip.enabled = true;
+            inVehicle = true;
+            mainCam.enabled = false;
+            driveCam.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+          //  guiObj.SetActive(false);
+        }
+    }
+
+}
---
+++ RPG Game/Assets/scripts/vehicles/Vehicle.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour {
+
+    public float speed = 100.0F;
+    public float rotateSpeed = 5.0F;
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    public GameObject bulletPrefabMG;
+    public Transform bulletSpawnMG;
+
+    AudioSource source;
+    public AudioClip audioMain;
+    public AudioClip audioMG;
+
+    public float bulletSpeed = 1000.0F;
+    public float reloadTime = 5.0F;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * rotateSpeed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, -z);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+            Debug.Log("Fire1-Button");
+        }
+        if (Input.GetButton("Fire2"))
+        {
+            FireMG();
+            Debug.Log("Fire2-Button");
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Jump-Button");
+        }
+    }
+
+    public void Fire()
+    {
+        source = GetComponent<AudioSource>();
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+                bulletPrefab,
+                bulletSpawn.position,
+                bulletSpawn.rotation);
+
+            // Add velocity to the bullet
+            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+        
+            // Destroy the bullet after 0.2 seconds
+            Destroy(bullet, 0.2f);
+            source.PlayOneShot(audioMain, 1);
+           
+    }
+
+    public void FireMG()
+    {
+        source = GetComponent<AudioSource>();
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+                bulletPrefabMG,
+                bulletSpawnMG.position,
+                bulletSpawnMG.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+
+        // Destroy the bullet after 0.2 seconds
+        Destroy(bullet, 0.2f);
+        source.PlayOneShot(audioMG, 1);
+
+    }
+
+
+}
