2de3bcd512d7eed7907e0a887758f568297c6314
unknown <patrick.nibbia@gmail.com>
2020-09-12 14:02:38 +0200

    feat: add player

---
+++ RPG Game/Assets/scripts/camera/MouseLook.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+    Vector2 mouseLook;
+    Vector2 smoothV;
+
+    public float sensitivity = 2.0F;
+    public float smoothing = 2.0F;
+
+    public float minRot = -70.0F;
+    public float maxRot = +70.0F;
+
+    GameObject character;
+	// Use this for initialization
+	void Start ()
+    {
+        character = this.transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Clamp(smoothV.x, minRot, maxRot); // Doesn't working yet
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1F / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1F / smoothing);
+
+        mouseLook += smoothV;
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ RPG Game/Assets/scripts/player/CameraChange.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    public GameObject firstPersonCam;
+    public GameObject thirdPersonCam;
+    public int camMode;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (camMode == 1)
+            {
+                camMode = 0;
+            }
+            else
+            {
+                camMode += 1;
+            }
+            StartCoroutine(CamChange());
+        }  
+    }
+
+    IEnumerator CamChange()
+    {
+        yield return new WaitForSeconds(0.01F);
+        if (camMode == 0)
+        {
+            firstPersonCam.SetActive(true);
+            thirdPersonCam.SetActive(false);
+        }
+        if (camMode == 1)
+        {
+            firstPersonCam.SetActive(false);
+            thirdPersonCam.SetActive(true);
+        }
+        
+    }
+}
